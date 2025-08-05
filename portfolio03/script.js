fc61bd4f46114589698cde27755ca4833c30a453
ibrahimM <ibrahimmustafa787898@gmail.com>
2025-08-05 03:15:47 -0400

    deleted portfolio03

--- portfolio03/script.js
+++
@@ -1,6 +0,0 @@
-const cursor = document.querySelector(".cursor-shadow");
-
-  window.addEventListener("mousemove", (e) => {
-    cursor.style.left = e.clientX + "px";
-    cursor.style.top = e.clientY + "px";
-});
